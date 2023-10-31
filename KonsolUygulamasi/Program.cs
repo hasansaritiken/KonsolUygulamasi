b97208e66ac36f8e3627636e57c5995ffe0e71cf
Hüseyin <Hseyin@DESKTOP-9MC0EJ4>
2023-10-31 10:24:09 +0300

    Proje dosyası ekle.

---
+++ KonsolUygulamasi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("herkese selam arkadaşlar ");
+            Console.WriteLine("Ben dj dikkat");
+        }
+    }
+}
